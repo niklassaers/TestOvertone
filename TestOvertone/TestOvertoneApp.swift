40f4eae575ec0d06306d2e884b96fc659e989a42
Niklas Saers <niklas@saers.com>
2021-03-25 14:15:54 +0100

    Initial Commit

---
+++ TestOvertone/TestOvertoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestOvertoneApp.swift
+//  TestOvertone
+//
+//  Created by Niklas @Home on 25/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestOvertoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
